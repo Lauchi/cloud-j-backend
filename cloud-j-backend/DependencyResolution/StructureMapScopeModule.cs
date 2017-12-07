84b118965b83ba2f6d1d1a0dbdf49cc368792207
Lauchi <simonheiss87@gmail.com>
2017-12-07 21:29:56 +0100

    added ioc frameworks

---
+++ cloud-j-backend/DependencyResolution/StructureMapScopeModule.cs
@@ -0,0 +1,24 @@
+namespace cloud_j_backend.DependencyResolution {
+    using System.Web;
+
+    using cloud_j_backend.App_Start;
+
+    using StructureMap.Web.Pipeline;
+
+    public class StructureMapScopeModule : IHttpModule {
+        #region Public Methods and Operators
+
+        public void Dispose() {
+        }
+
+        public void Init(HttpApplication context) {
+            context.BeginRequest += (sender, e) => StructuremapMvc.StructureMapDependencyScope.CreateNestedContainer();
+            context.EndRequest += (sender, e) => {
+                HttpContextLifecycle.DisposeAndClearAll();
+                StructuremapMvc.StructureMapDependencyScope.DisposeNestedContainer();
+            };
+        }
+
+        #endregion
+    }
+}
