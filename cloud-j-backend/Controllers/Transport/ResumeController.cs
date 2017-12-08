5f1cdc376f81f48a9a79ef20ffb85f515d58a68a
simon <simonheiss87@gmail.com>
2017-12-08 21:10:45 +0100

    resume controller added

---
+++ cloud-j-backend/Controllers/Transport/ResumeController.cs
@@ -0,0 +1,23 @@
+﻿using System.Web.Http;
+using AudioEngine.Domain;
+
+namespace cloud_j_backend.Controllers.Transport
+{
+    [Route("resume")]
+    public class ResumeController : ApiController
+    {
+        public IMixer Mixer { get; }
+
+        public ResumeController(IMixer mixer)
+        {
+            Mixer = mixer;
+        }
+
+        [HttpPost]
+        public IHttpActionResult Play(int channelId )
+        {
+            Mixer.Channels[channelId - 1].Position = 0;
+            return Ok(Mixer.Channels[channelId - 1].Position);
+        }
+    }
+}
